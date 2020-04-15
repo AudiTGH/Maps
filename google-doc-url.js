45240ff7d95a8a08eb73d0beafecb8044f6bccda
TGHDataSudan <45471236+TGHDataSudan@users.noreply.github.com>
2020-04-15 14:06:35 +0200

    Add files via upload

---
+++ google-doc-url.js
@@ -0,0 +1,17 @@
+
+// returns params set in URL
+function getUrlVars() {
+  var vars = {};
+  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+      vars[key] = value;
+  });
+  return vars;
+}
+
+var urls = [];
+urls.push('https://docs.google.com/spreadsheets/d/1-13FgMNZeeuIOrEVenv3qhKwEt2_NflG_7FQKGY9KoE/edit#gid=0');
+urls.push('https://docs.google.com/spreadsheets/d/1-13FgMNZeeuIOrEVenv3qhKwEt2_NflG_7FQKGY9KoE/edit#gid=0');
+urls.push('https://docs.google.com/spreadsheets/d/1-13FgMNZeeuIOrEVenv3qhKwEt2_NflG_7FQKGY9KoE/edit#gid=0');
+
+var number = getUrlVars()["u"];
+var googleDocURL = urls[number];
