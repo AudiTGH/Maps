0f77b2fbdfe2e592ccc71a67a6134f5dfee5f367
AudiTGH <63733458+AudiTGH@users.noreply.github.com>
2020-04-16 10:06:26 +0200

    Update gsheets.js

--- gsheets.js
+++ gsheets.js
@@ -11,6 +11,7 @@ function getUrlVars() {
 var urls = [];
 urls.push('https://docs.google.com/spreadsheets/d/1-13FgMNZeeuIOrEVenv3qhKwEt2_NflG_7FQKGY9KoE/edit#gid=0');
 urls.push('https://docs.google.com/spreadsheets/d/1-13FgMNZeeuIOrEVenv3qhKwEt2_NflG_7FQKGY9KoE/edit#gid=0');
+urls.push('https://docs.google.com/spreadsheets/d/1-13FgMNZeeuIOrEVenv3qhKwEt2_NflG_7FQKGY9KoE/edit#gid=0');
 urls.push('https://docs.google.com/spreadsheets/d/1eo_E_hnhZ4VVXIVHFTAcVUxkgJjwQOB0buZ5CFmasF4/edit?t#gid=0');
 
 var number = getUrlVars()["u"];
